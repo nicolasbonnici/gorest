421bb1ca2c7bf869f99a50faef64c7013e694966
Nicolas Bonnici <nicolasbonnici@gmail.com>
2025-11-22 18:00:58 +0100

    chore: fix gorest configuration in basic api example and wrap with auth todo routes

--- cmd/benchmark/testserver/main.go
+++ cmd/benchmark/testserver/main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"github.com/nicolasbonnici/gorest"
+	"github.com/nicolasbonnici/gorest/pluginloader"
 	"github.com/nicolasbonnici/gorest/test/generated/resources"
+
+	authplugin "github.com/nicolasbonnici/gorest/plugins/auth"
+	contenttypeplugin "github.com/nicolasbonnici/gorest/plugins/contenttype"
+	corsplugin "github.com/nicolasbonnici/gorest/plugins/cors"
+	loggerplugin "github.com/nicolasbonnici/gorest/plugins/logger"
+	ratelimitplugin "github.com/nicolasbonnici/gorest/plugins/ratelimit"
+	requestidplugin "github.com/nicolasbonnici/gorest/plugins/requestid"
+	securityplugin "github.com/nicolasbonnici/gorest/plugins/security"
 )
+
+func init() {
+	pluginloader.RegisterGlobalPluginFactory("requestid", requestidplugin.NewPlugin)
+	pluginloader.RegisterGlobalPluginFactory("logger", loggerplugin.NewPlugin)
+	pluginloader.RegisterGlobalPluginFactory("cors", corsplugin.NewPlugin)
+	pluginloader.RegisterGlobalPluginFactory("ratelimit", ratelimitplugin.NewPlugin)
+	pluginloader.RegisterGlobalPluginFactory("security", securityplugin.NewPlugin)
+	pluginloader.RegisterGlobalPluginFactory("contenttype", contenttypeplugin.NewPlugin)
+	pluginloader.RegisterRoutePluginFactory("auth", authplugin.NewPlugin)
+}
 
 func main() {
 	cfg := gorest.Config{
-		ConfigPath:     ".",
+		ConfigPath:     "./cmd/benchmark/testserver",
 		RegisterRoutes: resources.RegisterGeneratedRoutes,
 	}
 
