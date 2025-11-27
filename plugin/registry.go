bab57885829c5259869313ded7ba3d06a27f9f19
Nicolas Bonnici <nicolasbonnici@gmail.com>
2025-11-27 22:17:24 +0100

    chore: remove useless comments

--- plugin/registry.go
+++ plugin/registry.go
@@ -1,6 +1,5 @@
 package plugin
 
-// PluginRegistry stores all loaded plugins.
 // Plugins are NOT automatically applied; user must explicitly call plugin.Handler() and register it.
 type PluginRegistry struct {
 	plugins map[string]Plugin
@@ -12,19 +11,15 @@ func NewPluginRegistry() *PluginRegistry {
 	}
 }
 
-// Register adds a plugin to the registry.
-// Does NOT apply the plugin to any routes - that must be done manually.
 func (r *PluginRegistry) Register(plugin Plugin) {
 	r.plugins[plugin.Name()] = plugin
 }
 
-// Get retrieves a plugin by name.
 func (r *PluginRegistry) Get(name string) (Plugin, bool) {
 	plugin, exists := r.plugins[name]
 	return plugin, exists
 }
 
-// GetAll returns all registered plugins.
 func (r *PluginRegistry) GetAll() map[string]Plugin {
 	return r.plugins
 }
