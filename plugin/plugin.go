6bea2e0f79a455b3d7e300757a4cdece2ce95184
Nicolas Bonnici <nicolasbonnici@gmail.com>
2025-12-03 23:46:12 +0100

    docs: remove useless comments

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -12,12 +12,10 @@ type Plugin interface {
 	Handler() fiber.Handler
 }
 
-// EndpointSetup is an optional interface for plugins that need to register endpoints.
 type EndpointSetup interface {
 	SetupEndpoints(app *fiber.App) error
 }
 
-// Command represents a CLI command provided by a plugin
 type Command interface {
 	// Name returns the command name (e.g., "models", "resources")
 	Name() string
@@ -29,38 +27,26 @@ type Command interface {
 	Run(ctx *CommandContext) *CommandResult
 }
 
-// CommandContext provides context and dependencies for command execution
 type CommandContext struct {
-	// Config is the application configuration
 	Config interface{}
 
-	// Args are command-line arguments passed to the command
 	Args []string
 
-	// ProgressCallback is called to report progress
 	ProgressCallback func(message string)
 }
 
-// CommandResult contains the result of command execution
 type CommandResult struct {
-	// Success indicates whether the command succeeded
 	Success bool
 
-	// Error contains the error if the command failed
 	Error error
 
-	// FilesCreated lists all files created by the command
 	FilesCreated []string
 
-	// FilesModified lists all files modified by the command
 	FilesModified []string
 
-	// Message contains additional information about the result
 	Message string
 }
 
-// CommandProvider is an optional interface for plugins that provide CLI commands
 type CommandProvider interface {
-	// Commands returns all commands provided by this plugin
 	Commands() []Command
 }
